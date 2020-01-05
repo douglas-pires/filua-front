0d996751ca37238d49d7bdf50c8b8bb54e6b3e75
Douglas Pires <douglas.pires@live.com>
2020-01-05 20:19:35 -0300

    add validations object

---
+++ helpers/validations.ts
@@ -0,0 +1,7 @@
+export default {
+  required: (value: string) => !!value || 'Required.',
+  email: (value: string) => {
+    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return pattern.test(value) || 'Invalid email.'
+  }
+}
